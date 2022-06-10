5bc5cad83ac03d2fbc94ad4da8aa21e5589d9d72
Federico Capaldo <federico.capaldo1@gmail.com>
2022-06-10 01:48:31 +0100

    tdd 4: added code for full_name function in users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def full_name
+    "#{first_name.capitalize} #{last_name.capitalize}"
+  end
 end
