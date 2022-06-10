dfed97ef748ed729d45ff11610fba23b33c6e750
Federico Capaldo <federico.capaldo1@gmail.com>
2022-06-10 01:47:45 +0100

    tdd 3: added test for full_name function in users

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "full_name returns the capitalized first name and last name" do
+    user = User.new(first_name: "john", last_name: "lennon")
+    assert_equal "John Lennon", user.full_name
+  end
 end
