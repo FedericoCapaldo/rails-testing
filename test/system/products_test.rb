ec36a3efb0c4a62814eef7cd3336348aced08448
Federico Capaldo <federico.capaldo1@gmail.com>
2022-06-10 01:04:00 +0100

    tdd 1: added test before code

--- test/system/products_test.rb
+++ test/system/products_test.rb
@@ -5,7 +5,6 @@ class ProductsTest < ApplicationSystemTestCase
   test "visiting the index" do
     visit root_url # "/"
 
-    save_and_open_screenshot
     assert_selector "h1", text: "Awesome Products"
   end
 
@@ -13,5 +12,21 @@ class ProductsTest < ApplicationSystemTestCase
     visit root_url # "/"
 
     assert_selector ".card-product", count: Product.count
+  end
+
+
+  test "let a signed in user create a new product" do
+    login_as users(:george) # Warden Helper function login_as
+    visit "/products/new"
+    save_screenshot
+
+    fill_in "product_name", with: "Le Wagon"
+    fill_in "product_tagline", with: "Change your life: Learn to code"
+    save_screenshot
+
+    click_on "Create Product"
+    save_screenshot
+
+    assert_text "Change your life: Learn to code"
   end
 end
