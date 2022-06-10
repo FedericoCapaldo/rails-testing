4324ab984891560ad1906c5f9f207352144c40b1
Federico Capaldo <federico.capaldo1@gmail.com>
2022-06-10 01:27:58 +0100

    tdd 2: added code to pass test

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,4 +4,24 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render "new"
+    end
+  end
+
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :tagline)
+  end
 end
--- test/system/products_test.rb
+++ test/system/products_test.rb
@@ -18,14 +18,11 @@ class ProductsTest < ApplicationSystemTestCase
   test "let a signed in user create a new product" do
     login_as users(:george) # Warden Helper function login_as
     visit "/products/new"
-    save_screenshot
 
     fill_in "product_name", with: "Le Wagon"
     fill_in "product_tagline", with: "Change your life: Learn to code"
-    save_screenshot
 
     click_on "Create Product"
-    save_screenshot
 
     assert_text "Change your life: Learn to code"
   end
